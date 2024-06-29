3b5773757f6d7de007ff510bfc36e9c7c49caae9
smilejakdu <ash982416@gmail.com>
2024-06-29 10:08:17 +0900

    first commit

---
+++ src/main/kotlin/com/bss/batchsettlementsystem/BatchSettlementSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.bss.batchsettlementsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BatchSettlementSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<BatchSettlementSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/bss/batchsettlementsystem/BatchSettlementSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bss.batchsettlementsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BatchSettlementSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
